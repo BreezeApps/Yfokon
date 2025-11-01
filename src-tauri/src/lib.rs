cd5aa91ea4dc05555b1798d6ca7af8c9b63b6372
Sn0wcode <dem9ngemarcel@laposte.net>
2025-11-01 16:25:48 +0100

    set WEBKIT_DISABLE_DMABUF_RENDERER environnement to 1 on linux

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,14 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//
+// Set WEBKIT_DISABLE_DMABUF_RENDERER on Linux; provide a no-op on other platforms.
+#[cfg(target_os = "linux")]
+fn set_webkit_env() {
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    set_webkit_env();
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_window_state::Builder::new().build())
