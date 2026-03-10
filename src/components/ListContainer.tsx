790847a3d98122919fcdd5b1954a08893492bc07
Sn0wcode <dem9ngemarcel@laposte.net>
2026-03-10 21:05:07 +0100

    feat(src/App.tsx): update App layout to improve visual consistency
    * Change the z-index of the about overlay to 49 to match the z-index of the tabs
    * Change the z-index of the about overlay to 49 to match the z-index of the tabs
    * Change the class of the about overlay to shrink-0 to match the class of the tabs
    * Change the class of the about overlay to shrink-0 to match the class of the tabs
    * Change the class of the about overlay to shrink-0 to match the class of the tabs
    * Change the class of the about overlay to shrink-0 to match the class of the tabs
    * Change the class of the about overlay to shrink-0 to match the class of the tabs
    
    feat(src/components/AboutPage.tsx): add support for opening email and website links in new tab
    
    * Add target="_blank" attribute to email and website links in AboutPage component
    * Add onClick event handler to email and website links in AboutPage component to open links in new tab using openUrl function
    
    fix(src/components/ListContainer.tsx): update ListContainer height to fit content
    
    * Change the height of ListContainer to h-fit min-h-full to fit the content
    
    fix(src/components/Tab.tsx): update Tab width to fit content
    
    * Change the max-width of Tabs to 25rem to fit the content
    
    fix(src/lib/locales/fr-FR.json): update "Make Backup" translation to "Sauvegarder les données"
    
    * Change the translation of "Make Backup" to "Sauvegarder les données" to better reflect the action's purpose.

--- src/components/ListContainer.tsx
+++ src/components/ListContainer.tsx
@@ -160,7 +160,7 @@ export function ListContainer({
   return (
     <div
       style={{ background: board.color === null ? "" : board.color }}
-      className="h-full"
+      className="h-fit min-h-full"
     >
       <div className="p-4 sm:p-6 sm:pt-2 sm:pl-12 h-fit overflow-x-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 min-w-fit">
